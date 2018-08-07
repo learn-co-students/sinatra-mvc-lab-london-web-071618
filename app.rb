8eba26e1a181e93cca467a066e021d83ec6d5692
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-07 08:42:42 +0100

    solution

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    user_text = params["user_phrase"]
+    if user_text != nil
+      obj = PigLatinizer.new
+      @result_piglatin = obj.piglatinize(user_text)
+    else
+      status 200
+    end
+    erb :piglatinize
+  end
+
 end
